584b55d449fa155c830b2b163d8638cfe0061503
Carl-Anton Ingmarsson <carin003@student.liu.se>
2011-10-06 08:55:54 +0200

    Exercise 4: add locking to free_map

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -4,9 +4,11 @@
 #include "filesys/file.h"
 #include "filesys/filesys.h"
 #include "filesys/inode.h"
+#include "threads/synch.h"
 
 static struct file *free_map_file;   /* Free map file. */
 static struct bitmap *free_map;      /* Free map, one bit per disk sector. */
+static struct lock free_lock;        /* Lock protecting the free map */
 
 /* Initializes the free map. */
 void
@@ -17,6 +19,8 @@ free_map_init (void)
     PANIC ("bitmap creation failed--disk is too large");
   bitmap_mark (free_map, FREE_MAP_SECTOR);
   bitmap_mark (free_map, ROOT_DIR_SECTOR);
+
+  lock_init (&free_lock);
 }
 
 /* Allocates CNT consecutive sectors from the free map and stores
@@ -26,6 +30,9 @@ free_map_init (void)
 bool
 free_map_allocate (size_t cnt, disk_sector_t *sectorp) 
 {
+  /* Take lock */
+  lock_acquire (&free_lock);
+
   disk_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
   if (sector != BITMAP_ERROR
       && free_map_file != NULL
@@ -36,6 +43,10 @@ free_map_allocate (size_t cnt, disk_sector_t *sectorp)
     }
   if (sector != BITMAP_ERROR)
     *sectorp = sector;
+
+  /* Release lock */
+  lock_release (&free_lock);
+
   return sector != BITMAP_ERROR;
 }
 
@@ -43,9 +54,15 @@ free_map_allocate (size_t cnt, disk_sector_t *sectorp)
 void
 free_map_release (disk_sector_t sector, size_t cnt)
 {
+  /* Take lock */
+  lock_acquire (&free_lock);
+
   ASSERT (bitmap_all (free_map, sector, cnt));
   bitmap_set_multiple (free_map, sector, cnt, false);
   bitmap_write (free_map, free_map_file);
+
+  /* Release lock */
+  lock_release (&free_lock);
 }
 
 /* Opens the free map file and reads it from disk. */
